d9c0fed7fa40839f53be6f244082b0d3dc9a9565
Quentin Neyraud <quentin.neyraud@gmail.com>
2018-06-13 21:34:58 +0200

    update rollup config & add commander

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+
 export default {
     input: 'src/main.js',
     output: {
-        file: './lib/moveImage.js',
+        file: './lib/moveImage.min.js',
         format: 'cjs'
-    }
-};
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        uglify()
+    ]
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log('test')
+import program from 'commander'
+
+program
+    .version('0.1.0')
+    .parse(process.argv)
